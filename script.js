cbd9cce0d0f5173f0b4a0c4d3fe6719f88e42cf9
smithniamyfse <126514992+smithniamyfse@users.noreply.github.com>
2023-03-05 15:08:47 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-
+function myFunction() {
+  var x = document.getElementById("boxbackdrop");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+  
+}
+function myFunction2() {
+  var x = document.getElementById("boxworkhistory");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
